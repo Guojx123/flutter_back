2670fba5c60f2833f1980a1b337cd0b7d34fc185
gino.gjx <gino.gjx@mobvoyage.com>
2021-04-12 14:52:28 +0800

    单例

--- lib/category/skill/patterns/singleton_create.dart
+++ lib/category/skill/patterns/singleton_create.dart
@@ -8,3 +8,25 @@ class SingletonPage extends StatelessWidget {
     );
   }
 }
+
+
+
+class Singleton {
+  /// 单例对象
+  static Singleton _instance;
+
+  /// 内部构造方法，可避免外部暴露构造函数，进行实例化
+  Singleton._internal();
+
+  /// 工厂构造方法，这里使用命名构造函数方式进行声明
+  factory Singleton.getInstance() => _getInstance();
+
+  /// 获取单例内部方法
+  static _getInstance() {
+    // 只能有一个实例
+    if (_instance == null) {
+      _instance = Singleton._internal();
+    }
+    return _instance;
+  }
+}
